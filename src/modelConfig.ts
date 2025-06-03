5a3516b8ee9abf1e595e9c6305d46614fc74439e
valmyrtavaes <valmyrtavares71@gmail.com>
2025-06-03 15:57:19 -0300

    modelConfig updated

--- src/modelConfig.ts
+++ src/modelConfig.ts
@@ -7,7 +7,7 @@ export const modulesConfig = {
     ManagementRecipes: false, //Gerenciamento de Receitas
     TrackStockProduct: false, //estoque    OperationCostRegister: false,
     WelcomeSaluteForm: false, //Saudações iniciais
-    ExpensesManegementList: true, //Despesas
+    ExpensesManegementList: false, //Despesas
     RequestManagementModule: false, //vendas
   },
   createdAt: '2025-06-02T00:00:00Z',
